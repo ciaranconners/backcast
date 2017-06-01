4416f76ad2e4655c4332d3caefdc1a70217e7ba2
Ciaran Martin Conners <ciaranconners@gmail.com>
2017-06-01 15:28:49 -0400

    Done with Tests Up to Videos Collection

--- src/views/videoPlayer.js
+++ src/views/videoPlayer.js
@@ -1,9 +1,18 @@
 var VideoPlayerView = Backbone.View.extend({
 
+  initialize: function() {
+    this.render();
+  },
 
   render: function() {
     this.$el.html('<div class="loading">Please wait...</div>');
     return this;
+  },
+
+  events: {'select .embed-responsive-item': 'handleClick'},
+
+  handleClick: function() {
+    this.model.render();
   },
 
   template: templateURL('src/templates/videoPlayer.html')
