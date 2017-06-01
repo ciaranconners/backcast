20899c78691326197a634a955947549e55926811
Ciaran Martin Conners <ciaranconners@gmail.com>
2017-06-01 15:11:25 -0400

    Up to Tests for Video Player View

--- src/views/videoList.js
+++ src/views/videoList.js
@@ -1,12 +1,40 @@
 var VideoListView = Backbone.View.extend({
 
+  initialize: function() {
+    // need to append <ul> element to VideoListView <div>
+    // need to use map to create each of the <li> elements
+    // containing the video list entries
+    // this.render();
+    this.listenTo(this.collection, 'sync', this.render); 
+  },
+
 
   render: function() {
+    // console.log('this', this);
+    // console.log('this.$el', this.$el);
+
     this.$el.children().detach();
-    this.$el.html(this.template());
+    this.$el.html(this.template());  // div
+    // console.log('this.$el.html(this.template())', this.$el.html(this.template())); // div
+    // IGNORE: videoList.html is a <ul> template of hard-coded <li>s
+     // var referenceToNewDOM = map over exampleVideoData
+        // return <div><h5><em>videoListEntry</em> view goes here</h5></div>
+        // (3-collection for reference)
+    this.$el.find('h5').append(this.collection.map(function(video) {
+      var video = new Video(video);
+      return new VideoListEntryView({model: video});
+    }));
     return this;
   },
 
   template: templateURL('src/templates/videoList.html')
 
 });
+
+// append takes an array of elements or a single element
+
+    // var ourVideoListView = new VideoListView();
+    // for (let i = 0; i <= window.exampleVideoData.length; i++) {
+    //   var listEntry = new VideoListEntryView();
+    //   ourVideoListView.render().append(listEntry);
+    // }
--- src/views/videoListEntry.js
+++ src/views/videoListEntry.js
@@ -1,8 +1,32 @@
 var VideoListEntryView = Backbone.View.extend({
 
+  initialize: function() {
+    this.render();
+
+    // select '.video-list-entry-title'
+    // this.find('.video-list-entry-title').on('select', function() {
+    //   this.model.select();
+    // });
+    // this.model.on('click', function() {
+    //   this.model.select();
+    // });
+  },
+
+  events: {'click .video-list-entry-title': 'handleClick'},
+
+  handleClick: function() {
+    this.model.select();
+  },
 
   render: function() {
-    this.$el.html(this.template());
+    // console.log(this.model.attributes);
+
+    this.$el.html(this.template(this.model.attributes));
+
+    // this.$el.html(this.template(this.model.attributes.snippet));
+    // ^ Attempt to access snippet attribute
+    // ^ referencing the id and snippet (snippet has all attributes) attributes for a Video model
+
     return this;
   },
 
