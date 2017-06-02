3ba44943145be19d7fdee6b115b0d7713d56f1c0
Ciaran Martin Conners <ciaranconners@gmail.com>
2017-06-01 22:08:32 -0400

    Completed Pre-API Reqs

--- src/views/videoPlayer.js
+++ src/views/videoPlayer.js
@@ -9,8 +9,18 @@ var VideoPlayerView = Backbone.View.extend({
     this.render();
   },
 
+  // selectVideo: function() {
+  //   this.model = this;
+  //   this.render();
+  // },
+
   render: function() {
-    this.$el.html(this.template(this.model.attributes));
+
+    if (this.model) {
+      this.$el.html(this.template(this.model.attributes));
+    } else {
+      this.$el.html('<div class="loading">Please wait...</div>');
+    }
     return this;
   },
 
